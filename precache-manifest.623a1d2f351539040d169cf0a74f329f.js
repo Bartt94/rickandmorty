2ef39f7cf9e040d2f81dceee5cb08a833454bea4
Bartolo <bartolo.garcia@ferreryasociados.mx>
2023-04-28 18:17:08 -0600

    Updates

---
+++ precache-manifest.623a1d2f351539040d169cf0a74f329f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8af691a842cf7665ec38",
+    "url": "/rickandmorty.github.io/static/css/main.80758d02.chunk.css"
+  },
+  {
+    "revision": "8af691a842cf7665ec38",
+    "url": "/rickandmorty.github.io/static/js/main.8af691a8.chunk.js"
+  },
+  {
+    "revision": "956090ea75eddc4a5476",
+    "url": "/rickandmorty.github.io/static/js/1.956090ea.chunk.js"
+  },
+  {
+    "revision": "eaeb9d3bde7865c21980",
+    "url": "/rickandmorty.github.io/static/css/2.cac5ead3.chunk.css"
+  },
+  {
+    "revision": "eaeb9d3bde7865c21980",
+    "url": "/rickandmorty.github.io/static/js/2.eaeb9d3b.chunk.js"
+  },
+  {
+    "revision": "feb0bb7646a6844aebd6",
+    "url": "/rickandmorty.github.io/static/js/runtime~main.feb0bb76.js"
+  },
+  {
+    "revision": "12c45b36603ba8840523a6e6233e086a",
+    "url": "/rickandmorty.github.io/index.html"
+  }
+];
